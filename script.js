a732c0815427071c5440b19099205a8985d3693a
Lisa <107352832+oddpharmacy@users.noreply.github.com>
2023-01-14 00:17:51 +0800

    Add 16x16 grid

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const container = document.querySelector('.container');
 
-function createDiv() {
-    const div = document.createElement('div');
-    container.appendChild(div);
+function createRow() {
+    const row = document.createElement('div');
+    for (let i = 0; i < 16; i++) {
+        const div = document.createElement('div');
+        div.textContent = "x";
+        div.style.cssText = 'width: 30px; height: 30px; border: 1px solid black';
+        row.appendChild(div);
+    }
+    container.appendChild(row);
+}
+
+for (let i = 0; i < 16; i++) {
+    createRow();
 }
